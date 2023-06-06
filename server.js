23a4b3c6dd6da434b55ec0e50f24de045c91df5a
CodeSnob98 <mukherjeebiswarup112233@gmail.com>
2023-06-07 01:58:44 +0530

    simplified server.js file

--- server.js
+++ server.js
@@ -1,60 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const hbs=require('hbs');
-const livereload=require('livereload')
-const connectLiveReload=require('connect-livereload');
-const app = express()
-const liveReloadServer=livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, 'views'))
+const hbs = require("hbs");
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+let { registerPartials } = require("./helpers/helpers");
+const response = require("./utils/data.json");
 
+// setting up view engine
+// mandatory step
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
+// serving making contents of the stylesheets folder as static files
+app.use(express.static(path.join(__dirname, "stylessheets")));
+
+app.get("/pdf", (req, res) => {
+  const main = path.join(__dirname, "views/layout.hbs");
+
+  console.log("Registering the partials");
+  registerPartials();
+
+  // console.log("Registering the helpers");
+  // registerBDTHelpers();
+  // console.log("Generating the template");
+
+  const bstTemplate = fs.readFileSync(main, "utf-8");
+  const template = hbs.handlebars.compile(bstTemplate);
+  console.log("Compiled the template");
+  const html = template({
+    ...response,
+  });
+  res.send(html);
 });
 
-var indexRouter = require('./routes/index');
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-hbs.registerPartials(path.join(__dirname, 'views'))
-
-hbs.registerPartials(__dirname+'/views')
-// hbs.registerHelper(helpers);
-
-
-app.use(connectLiveReload())
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'stylessheets')));
-
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-  });
-  
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
-  
-  app.listen(3000, ()=>{
-    console.log('Successfully started the server')
-  })
-  
-  module.exports = app;
+app.listen(3000, () => {
+  console.log("Successfully started the server");
+});
